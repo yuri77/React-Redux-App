729743f34429968e24806a9d10dd6bb01508a69e
yurika sakae <yurika.sun@gmail.com>
2019-08-15 15:23:44 -0700

    MVP complete . WIP:Styling + add more functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
+import JokeList from "./components/JokeList";
 
 function App() {
   return (
     <div className="App">
-      <h1>Programming Jokes</h1>
+      <h1>Pick Your Jokes</h1>
+      <JokeList />
     </div>
   );
 }
