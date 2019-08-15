57b8aaaa185e30204efd7ded332b870f46b9be09
yurika sakae <yurika.sun@gmail.com>
2019-08-15 14:18:39 -0700

    dependencies set up

--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ import "./index.css";
 import App from "./App";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { jokeReducer } from "./reducers";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  jokeReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
